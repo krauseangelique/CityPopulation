b1d068e7a37963b2f9474b52e60606a30fe3d210
angelique KRAUSE <krause_angelique@hotmail.com>
2022-08-11 12:06:00 +0200

    data recovory js

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
 // API connexion
-const endpoint =
+const url =
   "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 
-// 
+const searchInput = document.querySelector(".search");
+const outputSuggestions = document.querySelector(".suggestions");
 const cities = [];
 
-fetch(endpoint)
+fetch(url)
   .then((response) => response.json())
-  .then((datas) => cities.push(...datas));
+  .then((data) => cities.push(...data));
 
 console.log(cities);
 
-async function filterCities(array) {
-  await array.forEach((el) => {
-    console.log(el);
+function findMatchWord(wordToMatch, cities) {
+  cities.filter((word) => {
+    const regex = new RegExp(wordToMatch, "gi");
+    word.city.match(regex) || word.state.match(regex);
   });
-
 }
 
-filterCities(cities);
+// Write into the input
+function displayMatchWordHandler() {
+  const matchArray = findMatchWord(this.value, cities);
+
+  // create li span etc.
+  const htmlElement = matchArray
+    .map((place) => {
+
+      const regex = new RegExp(this.value, "gi");
+      const cityName = place.city.replace(
+        regex,
+        `<span class='hl'>${this.value}</span>`
+      );
+      const stateName = place.city.replace(
+        regex,
+        `<span class='hl'>${this.value}</span>`
+          `<li>
+        <p></p>
+        </li>`
+      );
+
+    })
+}
+
+searchInput.addEventListener("change", displayMatchWordHandler);
